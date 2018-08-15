672ea05edf98f494ba868dd2b405213240bdc2dc
isoppp <prog@d2deck.com>
2018-08-15 12:04:49 +0900

    modify migration/seed js format

--- seeds/01_user.js
+++ seeds/01_user.js
@@ -1,16 +1,12 @@
 const md5 = require('md5')
 
-exports.seed = function (knex, Promise) {
-  // Deletes ALL existing entries
-  return knex('user').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('user').insert([
-        {
-          name: 'Test User',
-          email: 'user@example.com',
-          password: md5('u53rtest'),
-        },
-      ]);
-    });
-};
+exports.seed = async (knex) => {
+  await knex('user').del()
+  await knex('user').insert([
+    {
+      name: 'Test User',
+      email: 'user@example.com',
+      password: md5('u53rtest'),
+    },
+  ])
+}
